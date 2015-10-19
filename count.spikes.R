37f4215a72d94d18245cf6bdd49f085a4d5a0de1
leyshock <leyshock@ohsu.edu>
2015-10-19 14:22:40 -0700

    fixed misc bugs, validated code in comparison to Matthews

--- count.spikes.R
+++ count.spikes.R
@@ -21,40 +21,39 @@ count.spikes <- function(input.fastq,
                             stringsAsFactors=FALSE,
                             sep=" ");
     spikes <- spike.table$SPIKE;
+    #   TODO:  add some error-checking, verifying that all spikes are of same length, etc.
 
     #   modify spikes as required
-    
+    if(direction == "REV")  {
+        #   first convert to a DNAString, to convert to reverse-complement,
+        #       then back to a character
+        #   I do not believe that DNAString is vectorized, so we loop
+       for(i in 1:length(spikes))   {
+           spikes[i] <- as.character(reverseComplement(DNAString(spikes[i]))); 
+       }   #   for i 
+    }   #   fi
+    #    trim to length (regardless of whether we're using the spike or its reverse-compl.
+    spikes <- strtrim(spikes, spike.length);
 
     #   create output
     output.table <- spike.table;
     output.table$spike.count <- 0;
 
     #   count spikes - note there are two cases
-#    for(i in 1:length(spikes))  {
-    for(i in 1:10)  {
+    for(i in 1:length(spikes))  {
         if((i %% 10) == 0)  {
             cat("Processing spike ", i, " out of ", length(spikes), "\n", sep="");
         }   #   fi
-        if(direction == "FWD")  {   #   use the spike as-is
-            #   TODO:  add some error-checking, e.g. with file name
-            current.spike <- spikes[i];
-        }   else {  #   use the reverse-complement of the spike in the spike file 
-            current.spike <- as.character(reverseComplement(DNAString(spikes[i])));
-            #   TODO:  add some error-checking, e.g. with file name
-        }   #   else
-
+        current.spike <- spikes[i];
         #   count spike occurences
         current.spike.counts <- vcountPattern(current.spike, sread(fastq.reads));
         output.table[i,]$spike.count <- sum(as.logical(current.spike.counts));
-
         #   record read ids (as they'll need removed later)
-
-        
     }   #   for i
 
     #   build sames of output files
-    count.table <- paste(output.dir, basename(spike.file), ".counts.txt", sep="");
-    reads.to.remove.list <- paste(output.dir, basename(spike.file), ".reads.to.remove.txt", sep="");
+    count.table <- paste(output.dir, basename(input.fastq), ".counts.txt", sep="");
+    reads.to.remove.list <- paste(output.dir, basename(input.fastq), ".reads.to.remove.txt", sep="");
     cat("Writing spike count file to: ", count.table, "\n", sep="");
     cat("Writing list of reads to remove to: ", reads.to.remove.list, "\n", sep="");
     
