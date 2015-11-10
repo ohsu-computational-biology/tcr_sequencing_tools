6f3bf12a52fa920085714cc9f4c96e6b470c6ef4
leyshock <leyshock@ohsu.edu>
2015-11-10 12:11:56 -0800

    revised optimized.remove.spikes.wrapper.R so that it can work with single reads.to.remove files (as will be the case when using PEAR)

--- optimized.remove.spikes.wrapper.R
+++ optimized.remove.spikes.wrapper.R
@@ -10,6 +10,8 @@
 #	Load required libraries
 library(stringr);
 
+merged.reads <- TRUE;
+
 #	Get command-line arguments
 arguments <- commandArgs(trailingOnly=TRUE);
 working.dir <- arguments[1];
@@ -23,31 +25,40 @@ reads.to.remove.suffix <- ".reads.to.remove.txt";
 files.in.dir <- list.files(working.dir);
 
 #	Prepare a list of files to process
-read.files <- str_detect(files.in.dir, reads.to.remove.suffix);
-read.files <- files.in.dir[read.files];
-read.files <- paste(working.dir, read.files, sep="");
+#   text file indicating reads to remove
+reads.to.remove.files <- str_detect(files.in.dir, reads.to.remove.suffix);
+reads.to.remove.files <- files.in.dir[reads.to.remove.files];
+reads.to.remove.files <- paste(working.dir, reads.to.remove.files, sep="");
+#   Untouched fastq files
 fastq.files <- str_detect(files.in.dir, fastq.suffix);
 fastq.files <- files.in.dir[fastq.files];
 fastq.files <- paste(working.dir, fastq.files, sep="");
-file.roots.1 <- unique(read.files);
+file.roots.1 <- unique(reads.to.remove.files);
 file.roots.2 <- unique(fastq.files);
 
 #	error-check
 if(length(file.roots.1) != length(file.roots.2))
-	stop("Mismatch between number of fastq files and number of read files in directory")
+	stop("Mismatch between number of fastq files and number of reads.to.remove.files in directory")
 
 for(i in 1:length(fastq.files))	{
-
-	#	list.files() returns files in alphabetical order.  We process three files at a time,
-	#		assuming this order (though also confirming the order)
-	curr.fastq <- fastq.files[i];
-	if((i %% 2) == 0)	{
-		curr.reads.forward <- read.files[i - 1];
-		curr.reads.reverse <- read.files[i];
-	}	else	{
-		curr.reads.forward <- read.files[i];
-		curr.reads.reverse <- read.files[i + 1];
-	}	#	else
+    #   get a fastq file to process
+    curr.fastq <- fastq.files[i];
+  
+    #   get the appropriate read.to.remove files 
+     if(merged.reads)    {
+        curr.reads.forward <- reads.to.remove.files[i];
+        curr.reads.reverse <- reads.to.remove.files[i];
+    }   else {  #   for each fastq file there are two reads.to.remove.files, one forward, one reverse
+        #	list.files() returns files in alphabetical order.  We process three files at a time,
+        #		assuming this order (though also confirming the order)
+        if((i %% 2) == 0)	{
+            curr.reads.forward <- reads.to.remove.files[i - 1];
+            curr.reads.reverse <- reads.to.remove.files[i];
+        }	else	{
+            curr.reads.forward <- reads.to.remove.files[i];
+            curr.reads.reverse <- reads.to.remove.files[i + 1];
+        }	#	else
+    }   #   esle
 
 	#	call function
 	remove.fastqs(curr.fastq, curr.reads.forward, curr.reads.reverse);
