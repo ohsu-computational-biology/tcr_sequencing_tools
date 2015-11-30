9d319d5ea59a715df533d0ff78811e8d4d7929f4
leyshock <leyshock@ohsu.edu>
2015-11-30 12:23:17 -0800

    misc debug

--- normalize/normalize.clonotype.counts.condor.R
+++ normalize/normalize.clonotype.counts.condor.R
@@ -20,6 +20,72 @@ spike.count.file <- arguments[2];
 scaling.factor.file <- arguments[3];
 
 
+#   TODO:  remove the dependency on column order, since this isn't guaranteed
+#   TODO:  add some immediate checks to verify that the order is as we expect it to be
+postprocess.normalization.output <- function(input.table)   {
+
+    #  delete various columns that VDJTools does not expect (and possibly cannot handle)
+    input.table$Clone.count <- NULL;
+    input.table$Clone.fraction <- NULL;
+    input.table$V.segments <- NULL;
+    input.table$J.segments <- NULL;
+
+    new.colnames <- c("Clonal sequence(s)",
+                      "Clonal sequence quality(s)",
+                      "All V hits",
+                      "All D hits",
+                      "All J hits",
+                      "All C hits",  
+                      "All V alignment",
+                      "All D alignment",
+                      "All J alignment",
+                      "All C alignment",
+                      "N. Seq. FR1",
+                      "Min. qual. FR1",
+                      "N. Seq. CDR1",
+                      "Min. qual. CDR1",
+                      "N. Seq. FR2",
+                      "Min. qual. FR2",
+                      "N. Seq. CDR2",
+                      "Min. qual. CDR2",
+                      "N. Seq. FR3",
+                      "Min. qual. FR3",
+                      "N. Seq. CDR3",
+                      "Min. qual. CDR3",
+                      "N. Seq. FR4",
+                      "Min. qual. FR4",
+                      "AA. seq. FR1",
+                      "AA. seq. CDR1",
+                      "AA. seq. FR2",
+                      "AA. seq. CDR2",
+                      "AA. seq. FR3",
+                      "AA. seq. CDR3",
+                      "AA. seq. FR4",
+                      "Best V hit",
+                      " Best J hit", 
+                      "Clone count", 
+                      "Clone fraction");
+
+    #   assign new column names to data frame
+    colnames(input.table) <- new.colnames;
+
+    #   convert floating point counts to integers
+    input.table$"Clone count" <- round(input.table$"Clone count");
+    #   convert "Inf" values to zeroes
+    indices.to.change <- which(input.table$"Clone fraction" == Inf);
+    if(length(indices.to.change) > 0)   {
+        input.table[indices.to.change,]$"Clone fraction" <- 0;
+    }   #   fi
+    indices.to.change <- which(input.table$"Clone count" == Inf);
+    if(length(indices.to.change) > 0)   {
+        input.table[indices.to.change,]$"Clone count" <- 0;
+    }   #   fi
+
+    return(input.table);
+
+}   #   postprocess.normalization.output()   
+
+
   # Reads in the spiked_read counts
 #   TODO:  can we make this the spike file, rather than a particular count file?
   spiked_reads <- read.csv(spike.count.file, header = TRUE, stringsAsFactors = FALSE)
@@ -88,72 +154,6 @@ scaling.factor.file <- arguments[3];
                 sep="\t");
  
  
-
-
-#   TODO:  remove the dependency on column order, since this isn't guaranteed
-#   TODO:  add some immediate checks to verify that the order is as we expect it to be
-postprocess.normalization.output <- function(input.table)   {
-
-    #  delete various columns that VDJTools does not expect (and possibly cannot handle)
-    input.table$Clone.count <- NULL;
-    input.table$Clone.fraction <- NULL;
-    input.table$V.segments <- NULL;
-    input.table$J.segments <- NULL;
-
-    new.colnames <- c("Clonal sequence(s)",
-                      "Clonal sequence quality(s)",
-                      "All V hits",
-                      "All D hits",
-                      "All J hits",
-                      "All C hits",  
-                      "All V alignment",
-                      "All D alignment",
-                      "All J alignment",
-                      "All C alignment",
-                      "N. Seq. FR1",
-                      "Min. qual. FR1",
-                      "N. Seq. CDR1",
-                      "Min. qual. CDR1",
-                      "N. Seq. FR2",
-                      "Min. qual. FR2",
-                      "N. Seq. CDR2",
-                      "Min. qual. CDR2",
-                      "N. Seq. FR3",
-                      "Min. qual. FR3",
-                      "N. Seq. CDR3",
-                      "Min. qual. CDR3",
-                      "N. Seq. FR4",
-                      "Min. qual. FR4",
-                      "AA. seq. FR1",
-                      "AA. seq. CDR1",
-                      "AA. seq. FR2",
-                      "AA. seq. CDR2",
-                      "AA. seq. FR3",
-                      "AA. seq. CDR3",
-                      "AA. seq. FR4",
-                      "Best V hit",
-                      " Best J hit", 
-                      "Clone count", 
-                      "Clone fraction");
-
-    #   assign new column names to data frame
-    colnames(input.table) <- new.colnames;
-
-    #   convert floating point counts to integers
-    input.table$"Clone count" <- round(input.table$"Clone count");
-    #   convert "Inf" values to zeroes
-    indices.to.change <- which(input.table$"Clone fraction" == Inf);
-    if(length(indices.to.change) > 0)   {
-        input.table[indices.to.change,]$"Clone fraction" <- 0;
-    }   #   fi
-    indices.to.change <- which(input.table$"Clone count" == Inf);
-    if(length(indices.to.change) > 0)   {
-        input.table[indices.to.change,]$"Clone count" <- 0;
-    }   #   fi
-
-    return(input.table);
-
-}   #   postprocess.normalization.output()   
 
 
   
