b7a1da9836225267302dcb38b9a4eb522cc7c617
leyshock <leyshock@ohsu.edu>
2015-12-28 13:49:41 -0800

    cleaned up formatting by parameterizing the direction and base pair length

--- condor_tools/format.count.spikes.condor.R
+++ condor_tools/format.count.spikes.condor.R
@@ -7,32 +7,26 @@
 #
 #   After the script is run, paste the output into an applicable condor.submit file
 
-format.for.condor <- function(list.of.files) {
+format.for.condor <- function(list.of.files, bp="9bp", direction="fwd") {
 
     formatted.vector <- NULL;
+    bp.and.direction <- paste(bp, ".", direction, sep="");
   
     for (i in 1:length(list.of.files))   {
 
        formatted.vector[i] <- paste(
-            "output=$(script_dir)/logs/stdout_count_spikes_parallel_9bp_fwd", i, ".out\n",
-#            "output=$(script_dir)/logs/stdout_count_spikes_parallel_9bp_rev", i, ".out\n",
-#            "output=$(script_dir)/logs/stdout_count_spikes_parallel_25bp_fwd", i, ".out\n",
-            "error=$(script_dir)/logs/stderr_count_spikes_parallel_9bp_fwd", i, ".out\n",
-#            "error=$(script_dir)/logs/stderr_count_spikes_parallel_9bp_rev", i, ".out\n",
-#            "error=$(script_dir)/logs/stderr_count_spikes_parallel_25bp_fwd", i, ".out\n",
-            "log=$(script_dir)/logs/count_spikes_parallel_9bp_fwd", i, ".log\n",
-#            "log=$(script_dir)/logs/count_spikes_parallel_9bp_rev", i, ".log\n",
-#            "log=$(script_dir)/logs/count_spikes_parallel_25bp_fwd", i, ".log\n",
-            "arguments=$(script_dir)count.spikes.wrapper.9bp.fwd.R ",
-#            "arguments=$(script_dir)count.spikes.wrapper.9bp.rev.R ",
-#            "arguments=$(script_dir)count.spikes.wrapper.25bp.fwd.R ",
+            "output=$(script_dir)/logs/stdout_count_spikes_parallel.", bp.and.direction, ".", i, ".out\n",
+            "error=$(script_dir)/logs/stderr_count_spikes_parallel.", bp.and.direction, ".", i, ".out\n",
+            "log=$(script_dir)/logs/count_spikes_parallel.", bp.and.direction, ".", i, ".log\n",
+            "arguments=$(script_dir)count.spikes.wrapper.", bp.and.direction, ".R ",
             "$(data_dir)", list.of.files[i], " ",
             "\nqueue 1\n",
             sep=""); 
     }   #   while
 
+    output.file.name <- paste("formatted.", bp.and.direction, ".txt", sep="");
     write.table(formatted.vector,
-                file="formatted.9bp.fwd.txt",
+                file=output.file.name,
                 row.names = FALSE,
                 col.names = FALSE,
                 quote = FALSE);
