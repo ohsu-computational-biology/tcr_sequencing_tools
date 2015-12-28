41297e4e81577c810f49e3d3b1a9c30a24fe5ff1
leyshock <leyshock@ohsu.edu>
2015-12-28 10:04:31 -0800

    revised wrapper script to match changes to main script file

--- postprocess/frameshift.and.stop.remover.wrapper.R
+++ postprocess/frameshift.and.stop.remover.wrapper.R
@@ -3,6 +3,7 @@
 #	Get command-line arguments
 arguments <- commandArgs(trailingOnly=TRUE);
 working.dir <- arguments[1];
+remove.records <- arguments[2];
 
 #	Specify path to script
 #	For use on ExaCloud
@@ -15,7 +16,7 @@ files.in.dir <- list.files(working.dir);
 
 for(i in 1:length(files.in.dir))  {
 	#	call function
-	remove.records(file.path(working.dir, files.in.dir[i]));
+	postprocess.clone.table(file.path(working.dir, files.in.dir[i]), remove.records);
     cat("Processed: ", i, " (of ", length(files.in.dir), ")\n",sep="");
 
 }	#	for i
