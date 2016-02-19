a71e4f5b0375c845f2c11b3bd9d59cd5848f34b0
weshorton <hortowe@ohsu.edu>
2016-02-19 11:37:52 -0800

    updated log path
    changed path to work with new file system

--- condor_tools/format.remove.spikes.condor.R
+++ condor_tools/format.remove.spikes.condor.R
@@ -19,9 +19,9 @@ to.remove.forward <- arguments[2];  # /home/exacloud/lustre1/CompBio/data/tcrseq
     for (i in 1:length(sorted)) {
 
        formatted.vector[i] <- paste(
-            "output=$(log_dir)logs/stdout_remove_spikes_parallel_", i, ".out\n",
-            "error=$(log_dir)logs/stderr_remove_spikes_parallel_", i, ".out\n",
-            "log=$(log_dir)logs/remove_spikes_parallel_", i, ".log\n",
+            "output=$(log_dir)/stdout_remove_spikes_parallel_", i, ".out\n",
+            "error=$(log_dir)/stderr_remove_spikes_parallel_", i, ".out\n",
+            "log=$(log_dir)/remove_spikes_parallel_", i, ".log\n",
             "arguments=$(script_dir)tcr_sequencing_tools/process_spikes/remove.spikes.R ",
             "$(data_dir)", sorted[i], " ",
             "$(data_dir)", sorted.to.remove.forward[i], " ", 
