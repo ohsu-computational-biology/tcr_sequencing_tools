5bff5bfbd46410985386290cd35a4e6573a9188e
Wes Horton <hortowe@ohsu.edu>
2017-06-15 10:54:34 -0700

    Updated unpear counts spikes in order to compare results with peared count spikes

--- process_spikes/old_and_misc/unpear.count.spikes.R
+++ process_spikes/old_and_misc/unpear.count.spikes.R
@@ -10,10 +10,8 @@
 #       equals 9
 #
 #   load depdencies
-.libPaths("/mnt/lustre1/CompBio/lib/R/library")
 library(ShortRead);
-library(stringr);
-library(Biostrings);
+library(data.table);
 
 arguments <- commandArgs(trailingOnly=TRUE);
 input.fastq <- arguments[1];
@@ -24,114 +22,154 @@ direction <- arguments[5]; # generally "FWD"
 
 # Begin count.spikes function here
 
-    #   Read in fastq file
-    fastq.reads <- readFastq(input.fastq);
-    num.fastqs <- length(fastq.reads);
-    cat(num.fastqs, " fastq reads to process\n", sep="");
+###   Read in fastq file
+fastq.reads <- readFastq(input.fastq);
+num.fastqs <- length(fastq.reads);
+cat(num.fastqs, " fastq reads to process\n", sep="");
 
-    #   open and prepare spike file
-    spike.table <- read.csv(spike.file,
-                            stringsAsFactors=FALSE,
-                            sep=" ");
-    spikes <- spike.table$SPIKE;
-    #   TODO:  add some error-checking, verifying that all spikes are of same length, etc.
+###   open and prepare spike file
+spike.table <- fread(spike.file)
+spikes <- spike.table$SPIKE;
 
-    #   modify spikes as required
-    if(direction == "REV")  {
-        #   first convert to a DNAString, to convert to reverse-complement,
-        #       then back to a character
-        #   I do not believe that DNAString is vectorized, so we loop
-       for(i in 1:length(spikes))   {
-           spikes[i] <- as.character(reverseComplement(DNAString(spikes[i]))); 
-       }   #   for i 
-    }   #   fi
-    #    trim to length (regardless of whether we're using the spike or its reverse-compl.
-    spikes <- strtrim(spikes, spike.length);
 
-    #   create outputs
-    output.table <- spike.table;
-    output.table$spike.count <- 0;
-    records.to.remove.ids <- character();
+###   modify spikes as required
+if(direction == "REV")  {
+    ##   first convert to a DNAString, to convert to reverse-complement,
+    ##       then back to a character
+    ##   I do not believe that DNAString is vectorized, so we loop
+    spikes <- sapply(spikes, function(x) as.character(reverseComplement(DNAString(x))), USE.NAMES = F)
 
-    #   count spikes - note there are two cases
-    for(i in 1:length(spikes))  {
-       
-        #   get a single spike 
-        current.spike <- spikes[i];
-
-        #   count spike occurences
-        current.spike.counts <- vcountPattern(current.spike, sread(fastq.reads), max.mismatch = 1, with.indels = TRUE);
-        output.table[i,]$spike.count <- sum(as.logical(current.spike.counts));
-        cat("\t", output.table[i,]$spike.count, " spikes detected\n", sep="");
-
-        #   record readids (as they'll need removed later; we use the readid to do so)
-        records.to.remove <- which(current.spike.counts > 0);
-        if(length(records.to.remove) > 0)   {
-            temp.reads <- fastq.reads[records.to.remove];
-            records.to.remove.ids <- c(records.to.remove.ids, 
-                                        as.character(temp.reads@id));
-        }   #   fi
-        
-        #   In the case where spike.length == 9, all spikes are identical (since the
-        #       first nine characters of each spike are identical.  We use this break
-        #       to short-circuit the "for" loop, since we get all the information we
-        #       need in the first pass
-        #   TODO:  what if spikes don't share a common first nine characters?  Modify
-        #       the code to deal with this case
-        if(spike.length == 9)
-            break;
-
-        #   update progress to the user 
-        if((i %% 10) == 0)  {
-            cat("Processing spike ", i, " out of ", length(spikes), "\n", sep="");
-        }   #   fi
-    }   #   for i
-
-    #   construct summary for QC purposes
-    qc.summary <- data.frame(sample.id=character(),
-                             num.reads=integer(),
-                             num.spiked.reads=integer(),
-                             pct.spiked.reads=numeric(),
-                             stringsAsFactors=FALSE);
-    qc.summary[1,]$sample.id <- input.fastq;
-    qc.summary[1,]$num.reads <- num.fastqs;
-    qc.summary[1,]$num.spiked.reads <- sum(output.table$spike.count);
-    qc.summary[1,]$pct.spiked.reads <- (qc.summary$num.spiked.reads / num.fastqs) * 100;
-
-    #   modify output.table so it'll become easily incorporated into qc.summary
-    spike.count <- output.table$spike.count;
-    qc.spike <- data.frame(spike.count);
-    rownames(qc.spike) <- output.table$SPIKE_ID;
-    qc.spike <- t(qc.spike);
-    rownames(qc.spike) <- NULL;
-    qc.summary <- cbind(qc.summary, qc.spike); 
+    #for(i in 1:length(spikes))   {
+    #    spikes[i] <- as.character(reverseComplement(DNAString(spikes[i])));
+    #}   #   for i
     
-    #   build names of output files
-    #   strip ".fastq" from file name.  Not necessary, just more hygienic 
-    filename.no.extension <- sub("[.][^.]*$", "", basename(input.fastq));
-    count.table <- paste(output.dir, filename.no.extension, ".spike.counts.", spike.length, "bp.txt", sep="");
-	#	TODO:  strip .fastq away before appending ".reads.to.remove.txt"
-    reads.to.remove.list <- paste(output.dir, filename.no.extension, ".reads.to.remove.txt", sep="");
-    qc.file <- paste(output.dir, filename.no.extension, ".qc.txt", sep="");
-    cat("Writing spike count file to: ", count.table, "\n", sep="");
-    cat("Writing list of reads to remove to: ", reads.to.remove.list, "\n", sep="");
-    cat("Writing QC summary to: ", qc.file, "\n", sep="");
-    #   write outputs
-    write.table(output.table,
-                file=count.table,
-                quote=FALSE,
-                sep=",");
+}   #   fi
 
-    write.table(records.to.remove.ids,
-                file=reads.to.remove.list,
-                quote=FALSE,
-                row.names=FALSE,
-                col.names=FALSE);
+###    trim to length (regardless of whether we're using the spike or its reverse-compl.
+spikes <- strtrim(spikes, spike.length);
 
-    write.table(qc.summary,
-                file=qc.file,
-                quote=FALSE,
-                sep=",",
-                row.names=FALSE);
+###   create outputs
+output.table <- spike.table;
+output.table$spike.count <- 0;
+records.to.remove.ids <- character();
+
+###   count spikes - note there are two cases
+for(i in 1:length(spikes))  {
+    
+    ##   get a single spike
+    current.spike <- spikes[i];
+
+    if (spike.length == 9) {
+        ## Search reverse barcode as well
+        reverse.spike <- "GTCGACTTA"
+    } # fi
+    
+    ##   count spike occurences
+    ##   searches each fastq read for current spike, returns TRUE (found) or FALSE (not found)
+    current.spike.counts <- vcountPattern(current.spike,
+                                          sread(fastq.reads),
+                                          max.mismatch = 1,
+                                          with.indels = TRUE);
+
+    ## Count second spike occurrences
+    if (spike.length == 9){
+        reverse.spike.counts <- vcountPattern(reverse.spike,
+                                              sread(fastq.reads),
+                                              max.mismatch = 1,
+                                              with.indels = TRUE);
+        current.spike.counts <- current.spike.counts + reverse.spike.counts
+    } # fi
+
+    output.table[i,]$spike.count <- sum(as.logical(current.spike.counts));
+    cat("\t", output.table[i,]$spike.count, " spikes detected\n", sep="");
+    
+    ##   record read ids (as they'll need removed later; we use the readid to do so)
+    ##   Grab the indexes of all reads with spikes
+    records.to.remove <- which(current.spike.counts > 0);
+
+    ## If there are some reads, get their IDs from the fastq object
+    if(length(records.to.remove) > 0)   {
+        temp.reads <- fastq.reads[records.to.remove];
+        records.to.remove.ids <- c(records.to.remove.ids,
+                                   as.character(temp.reads@id));
+    }   #   fi
+        
+    ##   In the case where spike.length == 9, all spikes are identical (since the
+    ##       first nine characters of each spike are identical.  We use this break
+    ##       to short-circuit the "for" loop, since we get all the information we
+    ##       need in the first pass
+    ##   TODO:  what if spikes don't share a common first nine characters?  Modify
+    ##       the code to deal with this case
+    if(spike.length == 9)
+        break;
+
+    ##   update progress to the user
+    if((i %% 10) == 0)  {
+        cat("Processing spike ", i, " out of ", length(spikes), "\n", sep="");
+    }   #   fi
+    
+}   #   for i
+
+
+###   construct summary for QC purposes
+qc.summary <- data.frame(sample.id=character(),
+                         num.reads=integer(),
+                         num.spiked.reads=integer(),
+                         pct.spiked.reads=numeric(),
+                         stringsAsFactors=FALSE);
+
+qc.summary[1,]$sample.id <- input.fastq;
+qc.summary[1,]$num.reads <- num.fastqs;
+qc.summary[1,]$num.spiked.reads <- sum(output.table$spike.count);
+qc.summary[1,]$pct.spiked.reads <- (qc.summary$num.spiked.reads / num.fastqs) * 100;
+
+
+###   modify output.table so it'll become easily incorporated into qc.summary
+spike.count <- output.table$spike.count;
+qc.spike <- data.frame(spike.count);
+rownames(qc.spike) <- output.table$SPIKE_ID;
+qc.spike <- t(qc.spike);
+rownames(qc.spike) <- NULL;
+qc.summary <- cbind(qc.summary, qc.spike);
+
+### Change read vector to data.table
+records.to.remove.ids.dt <- as.data.table(records.to.remove.ids)
+colnames(records.to.remove.ids.dt) <- "Reads"
+
+
+###   build names of output files
+###   strip ".fastq" from file name.  Not necessary, just more hygienic
+filename.no.extension <- sub("[.][^.]*$", "", basename(input.fastq));
+count.table <- paste(output.dir, "counts/", filename.no.extension, ".spike.counts.", spike.length, "bp.txt", sep="");
+reads.to.remove.list <- paste(output.dir, "reads_to_remove/", filename.no.extension, ".reads.to.remove.txt", sep="");
+qc.file <- paste(output.dir, "qc/", filename.no.extension, ".qc.txt", sep="");
+
+### Update
+cat("Writing spike count file to: ", count.table, "\n", sep="");
+cat("Writing list of reads to remove to: ", reads.to.remove.list, "\n", sep="");
+cat("Writing QC summary to: ", qc.file, "\n", sep="");
+
+###  write outputs
+write.table(output.table,
+            file=count.table,
+            quote=FALSE,
+            row.names=FALSE,
+            sep="\t");
+
+
+write.table(records.to.remove.ids.dt,
+            file=reads.to.remove.list,
+            quote=FALSE,
+            row.names=FALSE,
+            col.names=FALSE,
+            sep='\t');
+
+
+write.table(qc.summary,
+            file=qc.file,
+            quote=FALSE,
+            sep="\t",
+            row.names=FALSE);
+
 
 
